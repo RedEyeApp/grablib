89bbf656a0bc1c05e39497acca58c6c58e51870a
cmsteffen <cmsteffen@haxys.net>
2021-01-20 22:32:36 -0600

    Quick revisions.

--- grablib/main.py
+++ grablib/main.py
@@ -7,7 +7,7 @@ import requests
 
 
 class GrabLib:
-    """Image Grabber: Downloads images from specified URLs."""
+    """Image Grabber: Downloads files from HTTP servers."""
 
     def __init__(self):
         """Initialize GrabLib."""
@@ -19,22 +19,22 @@ class GrabLib:
     def __exit__(self, exc_type, exc_value, exc_traceback):
         """Eliminate the Self."""
 
-    async def get(self, image_url):
+    async def get(self, file_url):
         """Retrieve the provided URL."""
         try:
             loop = asyncio.get_event_loop()
             response = await loop.run_in_executor(
-                None, requests.get, image_url
+                None, requests.get, file_url
             )
             data = response.content
             if response.status_code != 200:
-                image_sha256 = "0" * 64
+                file_sha256 = "0" * 64
                 data = f"Error: {response.status_code}".encode()
             else:
                 sha256 = hashlib.sha256()
                 sha256.update(response.content)
-                image_sha256 = sha256.hexdigest()
+                file_sha256 = sha256.hexdigest()
         except Exception as ex:
-            image_sha256 = "0" * 64
+            file_sha256 = "0" * 64
             data = f"Exception: {type(ex).__name__}".encode()
-        return {"url": image_url, "sha256": image_sha256, "data": data}
+        return {"url": file_url, "sha256": file_sha256, "data": data}
--- tests/test_main.py
+++ tests/test_main.py
@@ -10,27 +10,28 @@ pytestmark = pytest.mark.asyncio
 
 class TestGrabLib:
     """Test the GrabLib module."""
+    fake_url = ""
 
     async def test_get(self, mocker):
         """Test retrieval of URL."""
         mocker.patch("grablib.main.requests.get")
-        fake_image_data = b"\x00\x00\x00\x00"
+        fake_file_data = b"\x00\x00\x00\x00"
         response = mocker.MagicMock()
         response.status_code = 200
-        response.content = fake_image_data
+        response.content = fake_file_data
         grablib.main.requests.get.return_value = response
         with grablib.GrabLib() as grabber:
-            actual = await grabber.get("http://fake.com/image.jpg")
+            actual = await grabber.get(self.fake_url)
             expected = {
-                "url": "http://fake.com/image.jpg",
+                "url": self.fake_url,
                 "sha256": (
                     "df3f619804a92fdb4057192dc43dd748"
                     "ea778adc52bc498ce80524c014b81119"
                 ),
-                "data": fake_image_data,
+                "data": fake_file_data,
             }
         grablib.main.requests.get.assert_called_with(
-            "http://fake.com/image.jpg"
+            self.fake_url
         )
         assert actual == expected
 
@@ -43,9 +44,9 @@ class TestGrabLib:
         response.content = error_msg
         grablib.main.requests.get.return_value = response
         with grablib.GrabLib() as grabber:
-            actual = await grabber.get("http://fake.com/image.jpg")
+            actual = await grabber.get(self.fake_url)
             expected = {
-                "url": "http://fake.com/image.jpg",
+                "url": self.fake_url,
                 "sha256": "0" * 64,
                 "data": b"Error: 404",
             }
@@ -66,9 +67,9 @@ class TestGrabLib:
         ]
         with grablib.GrabLib() as grabber:
             for response_message in response_messages:
-                actual = await grabber.get("http://fake.com/image.jpg")
+                actual = await grabber.get(self.fake_url)
                 expected = {
-                    "url": "http://fake.com/image.jpg",
+                    "url": self.fake_url,
                     "sha256": "0" * 64,
                     "data": response_message,
                 }
